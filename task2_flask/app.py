14901f6c9dc0a1ad8f5ac0f11c9edef2ec26fcdf
kaper156 <156-oi@i.ua>
2020-09-15 19:52:43 +0600

    Task 2. The application passed a simple test
    A test is written that checks an eval_date with only one DF and one formula, without checking for bad scenarios
    The template is not dynamic: you cannot add additional formulas and enter datasets.

--- task2_flask/app.py
+++ task2_flask/app.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from flask import Flask, request, render_template, json
 
 app = Flask(__name__)
@@ -10,18 +11,56 @@ def index():
 
 @app.route('/eval_data', methods=['POST'])
 def eval_data():
-    ctx = {'error': '', 'result': ''}
-    expression = request.form['expression']
-    try:
-        ctx['result'] = eval(expression, {'__builtins__': {}})
-    except NameError as exc:
-        app.logger.error(exc)
-        ctx['error'] += f"Cannot access object in expression, check it please: {expression}\n"
-    except Exception as other_exc:
-        app.logger.error(other_exc)
-        ctx['error'] += f"Something went wrong"
-    finally:
-        return json.dumps(ctx)
+    # Get as json
+    json_data = json.loads(request.data)
+
+    dt_range = json_data['rangeStart'], json_data['rangeEnd']
+    # Dict for dataframes
+    data_frames = dict()
+    for name, df in json_data['df'].items():
+        # Make df from values
+        data_frames[name] = pd.DataFrame(df['values'], index=df['index'])
+        # Filter df by datetime range
+        data_frames[name] = data_frames[name].loc[dt_range[0]: dt_range[1]]
+
+    # Dict for graphs
+    graphs = json_data['graphs']
+    for graph in graphs:
+        # Eval with graphs with user choice data-frames
+        graph['result'] = eval(graph['formula'], {'__builtins__': {}}, data_frames)
+
+        # Convert them into simple python dict to put back
+        graph['result'] = graph['result'].to_dict()
+        # Bad trick to remove '0':
+        graph['result'] = graph['result'][0]
+
+    return json.dumps(graphs)
+
+    # print(request.form)
+    # ctx = {
+    #     'expressions_ctx': {request.form['expressions']: {'result': '', 'error': ''}, },
+    #     'rangeStart': request.form['rangeStart'],
+    #     'rangeEnd': request.form['rangeEnd'],
+    #
+    #
+    #     # {expr: {'result': '', 'error': ''} for expr in request.form['graph-formula']},
+    # }
+    #
+    # for expr, expr_ctx in ctx['expressions_ctx'].items():
+    #     try:
+    #         expr_ctx['result'] = eval(expr, {'__builtins__': {}})
+    #     except (NameError, KeyError) as exc:
+    #         app.logger.error(exc)
+    #         expr_ctx['error'] += f"Cannot access object in expression."
+    #     except ZeroDivisionError as exc:
+    #         app.logger.error(exc)
+    #         expr_ctx['error'] += f"Zero division is not allowed."
+    #     except Exception as other_exc:
+    #         app.logger.error(other_exc)
+    #         expr_ctx['error'] += f"Something went wrong during eval your expression."
+    #     finally:
+    #         expr_ctx['error'] = f"Error in your expression: {expr}<br>" + expr_ctx['error'] if expr_ctx['error'] else ''
+    # return json.dumps(ctx)
 
 
 if __name__ == '__main__':
--- task2_flask/static/js/script.js
+++ task2_flask/static/js/script.js
@@ -1,14 +1,52 @@
+$.ready(
+    function () {
+        var formulas_max = 6;
+        var formulas_counter = 1;
+        var tr_for_formulas = $('#add-formula');
+        var add_button = tr_for_formulas.select("a").first();
+
+
+        $(add_button).onclick(
+            function (e) {
+                console.log("ds"+add_button);
+                if (formulas_counter < formulas_max) {
+                    $("<tr><td>Formula</td><td><input type=\"text\" name=\"expressions[]\"></td>" +
+                        "<td><a href=\"#\" class=\"formula-delete\">Delete</a></td></tr>").insertBefore(tr_for_formulas);
+                    formulas_counter++;
+                //    TODO set on new formula formula-delete.onclick trigger
+                }
+            }
+        );
+        // TODO set on formulas formula-delete.onclick trigger
+
+    }
+);
+
+
 function eval_expression() {
+    var $form = $('#main-form'),
+        $error_p = $('#errors'),
+        $result_p = $('#result');
     $.ajax({
-        url: "/eval_data",
-        data: $('#enter-form').serialize(),
+        url: $form.attr('action'),
+        data: $form.serialize(),
         type: 'POST',
         success: function (response) {
             // Success mean got response from server
             var json = jQuery.parseJSON(response);
-            $('#errors').text(json.error); // Show error which can be raised in view
-            $('#result').text(json.result); // Show result of expression
+            $error_p.text('');
+            $result_p.text('');
+            for (var expression in json.expressions_ctx) {
+                var ctx = json.expressions_ctx[expression];
+                if (ctx.error) {
+                    $error_p.append(ctx.error);
+                } else {
+                    $result_p.append(expression + "=" + ctx.result);
+                }
+
+            }
             console.log(json);
+            return json;
         },
         error: function (error) {
             // Error mean: cannot get response from server
